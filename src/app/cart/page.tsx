65b4dcc3496870867bc0f5a41285e872ce8b75cf
bamikefa-adewale <bamikefaadewale@gmail.com>
2025-02-17 19:00:47 +0100

    done with cart page with cart summary

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -29,8 +29,7 @@ const CartPage = () => {
         <div className="my-20 flex-1 rounded-2xl p-10 bg-[#FCFFFC]">
           {carts?.length === 0 ? (
             <div>
-              <p>Nothing in cart</p>
-              {/* <EmptyCart /> */}
+              <EmptyCart />
             </div>
           ) : (
             <div className="space-y-6">
@@ -60,7 +59,7 @@ const CartPage = () => {
                     />
                   </div>
 
-                  <div className="overflow-x-auto w-[50%]">
+                  <div className="">
                     <table className="w-full border-collapse">
                       <thead>
                         <tr className="bg-gray-200">
@@ -83,6 +82,7 @@ const CartPage = () => {
                           <td className="border-l border-gray-300 px-6 py-3">
                             {item.name}
                           </td>
+
                           <td className="border-l border-gray-300 px-6 py-3 flex items-center">
                             <button
                               onClick={() => decreaseQuantity(item.id)}
@@ -118,8 +118,8 @@ const CartPage = () => {
                         </tr>
                       </tbody>
                     </table>
-                    <p className="text-gray-400 font-medium leading-5 py-3">
-                      <span className="text-base text-black capitalize">
+                    <p className="text-gray-400 font-medium leading-5 py-3 ">
+                      <span className="text-base text-black ml-10 capitalize">
                         Total:
                       </span>{" "}
                       ${item?.quantity * item?.currentPrice}
@@ -132,137 +132,24 @@ const CartPage = () => {
         </div>
 
         {/* Summary Section */}
-        {/* <div className="bg-slate-500 flex-4 w-1/3 p-6 text-white rounded-lg"> */}
-        <div className="my-20 flex-4 w-1/3 rounded-2xl h-[594px] p-10 bg-[#FCFFFC]">
-          <h2 className="text-xl font-semibold">Order Summary</h2>
-          <p className="mt-3 text-lg">
-            Total Items: <span className="font-bold">{totalItems}</span>
-          </p>
-          <p className="mt-2 text-lg">
-            Total Price:{" "}
-            <span className="font-bold">${totalCartPrice.toFixed(2)}</span>
-          </p>
-          <Button className="mt-4 w-full bg-green-600 hover:bg-green-700">
-            Proceed to Checkout
-          </Button>
-        </div>
+        {carts.length > 0 && (
+          <div className="my-20 flex-4 w-1/3 rounded-2xl p-10 bg-[#FCFFFC]">
+            <h2 className="text-xl font-semibold">Order Summary</h2>
+
+            <p className="mt-3 text-lg">
+              Total Items: <span className="font-bold">{totalItems}</span>
+            </p>
+            <p className="mt-2 text-lg">
+              Total Price:{" "}
+              <span className="font-bold">${totalCartPrice.toFixed(2)}</span>
+            </p>
+            <Button className="mt-4 w-full bg-green-600 hover:bg-green-700">
+              Proceed to Checkout
+            </Button>
+          </div>
+        )}
       </div>
     </Container>
-    // <Container>
-    //   <div className="flex">
-    //     <div className="my-20 flex-1 p-10 bg-[#FCFFFC]">
-    //       {carts?.length === 0 ? (
-    //         <div>
-    //           <p>nothing is cart</p>
-    //           {/* <EmptyCart /> */}
-    //         </div>
-    //       ) : (
-    //         <div className="space-y-6">
-    //           <div className="flex justify-between py-3">
-    //             <h1 className="text-2xl  font-normal">
-    //               Shopping Cart ({totalItems} items)
-    //             </h1>
-    //             <Button
-    //               onClick={clearCart}
-    //               className="mt-5 px-4 hover:bg-[#FCFFFC] hover:text-black "
-    //             >
-    //               Clear Cart
-    //             </Button>
-    //           </div>
-    //           {carts?.map((item) => (
-    //             <div
-    //               key={item.id}
-    //               className="border flex gap-20 justify-between rounded-lg bg-[#FCFFFC] shadow-lg"
-    //             >
-    //               <div>
-    //                 <Image
-    //                   src={item?.image}
-    //                   alt={item?.name}
-    //                   className="w-40 h-40 object-cover rounded-lg"
-    //                   width={60}
-    //                   height={60}
-    //                 />
-    //               </div>
-
-    //               <div className="overflow-x-auto w-[50%]">
-    //                 <table className="w-full border-collapse">
-    //                   <thead>
-    //                     <tr className="bg-gray-200">
-    //                       <th className="border-l border-gray-300 px-6 py-3 text-left">
-    //                         Name
-    //                       </th>
-    //                       <th className="border-l border-gray-300 px-6 py-3 text-left">
-    //                         Quantity
-    //                       </th>
-    //                       <th className="border-l border-gray-300 px-6 py-3 text-left">
-    //                         Price
-    //                       </th>
-    //                       <th className="border-l border-gray-300 px-6 py-3 text-left">
-    //                         Action
-    //                       </th>
-    //                     </tr>
-    //                   </thead>
-    //                   <tbody>
-    //                     <tr>
-    //                       <td className="border-l border-gray-300 px-6 py-3">
-    //                         {item.name}
-    //                       </td>
-    //                       <td className="border-l border-gray-300 px-6 py-3 flex items-center">
-    //                         <button
-    //                           onClick={() => decreaseQuantity(item.id)}
-    //                           className="px-3 py-1 bg-gray-300 hover:bg-gray-100 rounded-r"
-    //                         >
-    //                           <CircleMinus color="green" />
-    //                         </button>
-    //                         <span className="px-4">{item.quantity}</span>
-    //                         <button
-    //                           onClick={() => addToCart(item)}
-    //                           className="px-3 py-1 bg-gray-300 hover:bg-gray-100 rounded-r"
-    //                         >
-    //                           <CirclePlus color="green" />
-    //                         </button>
-    //                       </td>
-    //                       <td className="border-l border-gray-300 px-6 py-3">
-    //                         ${item.currentPrice}
-    //                       </td>
-    //                       <td className="border-l border-gray-300 px-6 py-3">
-    //                         <Button
-    //                           onClick={() => {
-    //                             removeFromCart(item.id);
-    //                             toast.error(`${item.name} Remove From Cart`);
-    //                           }}
-    //                           className="bg-red-500 hover:bg-gray-500 text-white px-3 py-1 flex gap-2 rounded"
-    //                         >
-    //                           Remove{" "}
-    //                           <span>
-    //                             <CopyX color="white" size={25} />
-    //                           </span>
-    //                         </Button>
-    //                       </td>
-    //                     </tr>
-    //                   </tbody>
-    //                 </table>
-    //                 <p className="text-gray-400 font-medium leading-5 py-3">
-    //                   <span className="text-base text-black capitalize">
-    //                     total
-    //                   </span>{" "}
-    //                   : {item?.quantity * item?.currentPrice}{" "}
-    //                 </p>
-    //               </div>
-    //             </div>
-    //           ))}
-    //         </div>
-    //       )}
-    //     </div>
-    //     <div className="bg-slate-500 flex-4">
-    //       {" "}
-    //       <p className="text-white">
-    //         {" "}
-    //         summary textstryuiop oiuytresdfghjkpiouiytdtfghjklpotytryuiopo
-    //       </p>{" "}
-    //     </div>
-    //   </div>
-    // </Container>
   );
 };
 
